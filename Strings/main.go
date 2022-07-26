d2bffe02281f0b19b60048c5188f55230ddf510d
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2022-07-26 19:22:14 +0400

    string basic

--- Strings/main.go
+++ Strings/main.go
@@ -33,4 +33,54 @@ func main() {
 	var root, _ = utf8.DecodeRuneInString(rootStr)
 	fmt.Printf("%c: %[1]v (%d bytes)\n", root, len(rootStr))
 	fmt.Println([]byte(rootStr))
+
+	fmt.Println("--------------------------------------")
+
+	// Строки
+	var emptyStr string
+	fmt.Println(emptyStr)
+
+	str0 := "peace0"
+	fmt.Println(str0)
+
+	var str1 = "peace1"
+	fmt.Println(str1)
+
+	var str2 string = "peace2"
+	fmt.Println(str2)
+
+	var unicodeStr string = "abcπγ本"
+	fmt.Println(unicodeStr)
+
+	var multilineStr1 = "line1\nline2"
+	fmt.Println(multilineStr1)
+
+	var multilineStr2 = `line3
+line4
+line5`
+	fmt.Println(multilineStr2)
+
+	var rawStr = `line1\nline2`
+	fmt.Println(rawStr)
+
+	fmt.Printf("ascii str[1]: %c\n", str0[1])
+	fmt.Printf("unicode str[1]: %c\n", unicodeStr[1])
+	fmt.Printf("unicode str[3:5]: %s\n", unicodeStr[3:5])
+	fmt.Printf("unicode str[7:10]: %s\n", unicodeStr[7:10])
+	//fmt.Println(emptyStr[1]) // index out of range
+
+	//unicodeStr[0] = 'd' // строки являются неизменяемыми
+
+	fmt.Printf("ascii str len: %d bytes, %d chars\n", len(str0), utf8.RuneCountInString(str0))
+	fmt.Printf("unicode str len: %d bytes, %d chars\n", len(unicodeStr), utf8.RuneCountInString(unicodeStr))
+
+	for _, c := range str0 {
+		fmt.Printf("%c ", c)
+	}
+	fmt.Println()
+
+	for _, c := range unicodeStr {
+		fmt.Printf("%c ", c)
+	}
+	fmt.Println()
 }
