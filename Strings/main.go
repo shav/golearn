359996c6de522e6b8014082dc4355361ef0be9e8
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2022-07-26 18:50:29 +0400

    characters

--- Strings/main.go
+++ Strings/main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
-	fmt.Printf("")
+	// Символы
+	aChar0 := 'A'
+	var aChar1 = 'A'
+	var aChar2 rune = 'A'
+	var star byte = '*'
+	fmt.Printf("%c%c%c%c\n", aChar0, aChar1, aChar2, star)
+
+	var pi rune = 960
+	var alpha rune = 940
+	var omega rune = 969
+	var bang byte = 33
+	fmt.Printf("%c%c%c%c\n", pi, alpha, omega, bang)
+
+	var str = string([]rune{pi, alpha, omega, rune(bang)})
+	fmt.Println(str)
+
+	var piStr = string(pi)
+	fmt.Println(piStr)
+
+	const gammaStr = "γ"
+	var gamma, _ = utf8.DecodeRuneInString(gammaStr)
+	fmt.Printf("%c: %[1]v\n", gamma)
+
+	const rootStr = "本"
+	var root, _ = utf8.DecodeRuneInString(rootStr)
+	fmt.Printf("%c: %[1]v (%d bytes)\n", root, len(rootStr))
+	fmt.Println([]byte(rootStr))
 }
