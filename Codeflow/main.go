d69cc7eef6b43d8d6658a6ff972eea3e8a4785bb
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2022-07-27 22:33:57 +0400

    conditions

--- Codeflow/main.go
+++ Codeflow/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Printf("")
+	// Условия
+	var year = 2100
+	if year%400 == 0 || (year%4 == 0 && year%100 != 0) {
+		fmt.Printf("%d year is leap\n", year)
+	} else {
+		fmt.Printf("%d year is not leap\n", year)
+	}
+
+	dayOfWeek := time.Now().Weekday()
+	if dayOfWeek == time.Sunday {
+		fmt.Println("Sunday")
+	} else if dayOfWeek == time.Saturday {
+		fmt.Println("Saturday")
+	} else {
+		fmt.Println("Workday")
+	}
 }
