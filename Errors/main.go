110a44362ba4944f90c29c5474d5d0cfdabcbc87
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2022-08-01 21:51:19 +0400

    panic

--- Errors/main.go
+++ Errors/main.go
@@ -8,8 +8,18 @@ func main() {
 	defer finish()
 	fmt.Println("Program has been started")
 	fmt.Println("Program is working")
+
+	fmt.Println(divide(15, 5))
+	fmt.Println(divide(4, 0)) // <-- panic
 }
 
 func finish() {
 	fmt.Println("Program has been finished")
+}
+
+func divide(x, y float64) float64 {
+	if y == 0 {
+		panic("Division by zero!")
+	}
+	return x / y
 }
