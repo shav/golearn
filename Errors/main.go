e6d732a310a40cf11607f8ff5723bf588230b260
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2022-08-01 22:16:52 +0400

    custom error types

--- Errors/main.go
+++ Errors/main.go
@@ -5,14 +5,34 @@ import (
 	"fmt"
 )
 
+type IndexOutOfRangeError struct {
+	message string
+	index   int
+}
+
+func (error IndexOutOfRangeError) Error() string {
+	return error.message
+}
+
+type ArgumentError struct {
+	message string
+	value   any
+}
+
+func (error ArgumentError) Error() string {
+	return error.message
+}
+
 func main() {
-	// defer
+	// Отложенный вызов функциий в конце программы
 	defer finish()
 	defer globalErrorHandler()
 	fmt.Println("Program has been started")
 	fmt.Println("Program is working")
 
-	// handle error
+	fmt.Println("---------------------------------")
+
+	// Обработка ошибок
 	q, err := divide2(1, 0)
 	if err != nil {
 		fmt.Println("divide2 error: ", err)
@@ -20,7 +40,31 @@ func main() {
 		fmt.Println(q)
 	}
 
-	// panic
+	fmt.Println("---------------------------------")
+
+	// Кастомные типы ошибок
+	err = raiseError(0)
+	if err != nil {
+		// Проверка типа ошибки (вариант 1):
+		if outOfRangeError, ok := err.(IndexOutOfRangeError); ok {
+			fmt.Printf("%s: %d\n", outOfRangeError.message, outOfRangeError.index)
+		}
+		if argumentError, ok := err.(ArgumentError); ok {
+			fmt.Printf("%s: %v\n", argumentError.message, argumentError.value)
+		}
+
+		// Проверка типа ошибки (вариант 2):
+		switch error := err.(type) {
+		case IndexOutOfRangeError:
+			fmt.Printf("%s: %d\n", error.message, error.index)
+		case ArgumentError:
+			fmt.Printf("%s: %v\n", error.message, error.value)
+		}
+	}
+
+	fmt.Println("---------------------------------")
+
+	// Паника
 	fmt.Println(divide(15, 5))
 	fmt.Println(divide(4, 0)) // <-- panic
 }
@@ -47,4 +91,14 @@ func globalErrorHandler() {
 	if e := recover(); e != nil {
 		fmt.Println("Error: ", e)
 	}
+}
+
+func raiseError(code int) error {
+	switch code {
+	case 0:
+		return IndexOutOfRangeError{message: "Index is out of range", index: 13}
+	case 1:
+		return ArgumentError{message: "Wrong argument value", value: "777"}
+	}
+	return nil
 }
