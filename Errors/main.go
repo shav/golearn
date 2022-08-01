62f289e229daef888664ae33068a139eb8fbd682
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2022-08-01 21:58:59 +0400

    return and handle error

--- Errors/main.go
+++ Errors/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
+	// defer
 	defer finish()
 	defer globalErrorHandler()
 	fmt.Println("Program has been started")
 	fmt.Println("Program is working")
 
+	// handle error
+	q, err := divide2(1, 0)
+	if err != nil {
+		fmt.Println("divide2 error: ", err)
+	} else {
+		fmt.Println(q)
+	}
+
+	// panic
 	fmt.Println(divide(15, 5))
 	fmt.Println(divide(4, 0)) // <-- panic
 }
@@ -23,6 +34,13 @@ func divide(x, y float64) float64 {
 		panic("Division by zero!")
 	}
 	return x / y
+}
+
+func divide2(x, y float64) (float64, error) {
+	if y == 0 {
+		return 0, errors.New("Division by zero!")
+	}
+	return x / y, nil
 }
 
 func globalErrorHandler() {
