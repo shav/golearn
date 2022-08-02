ad65b7750b75d33378c484226c200a8ce59dfcd6
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2022-08-02 21:22:01 +0400

    nested structures

--- Structures/main.go
+++ Structures/main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 )
 
+type Contact struct {
+	email string
+	phone string
+}
+
 type Person struct {
-	name string
-	age  int
+	name    string
+	age     int
+	contact Contact
 }
 
 func main() {
@@ -19,6 +25,23 @@ func main() {
 
 	person = Person{name: "Artem", age: 31}
 	fmt.Println(person)
+
+	fmt.Println("--------------------------------------")
+
+	// Вложенные структуры
+	var tom = Person{
+		name: "Tom",
+		age:  24,
+		contact: Contact{
+			email: "tom@gmail.com",
+			phone: "+1234567899",
+		},
+	}
+	fmt.Println(tom)
+	fmt.Println(tom.contact.phone)
+
+	tom.contact.phone = "+7(123)456-78-90"
+	fmt.Println(tom)
 
 	fmt.Println("--------------------------------------")
 }
