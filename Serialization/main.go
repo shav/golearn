8e2c100565304769dbd25e6f9c6755b8c7ad3163
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2022-08-04 14:02:53 +0400

    deserialize dynamic object

--- Serialization/main.go
+++ Serialization/main.go
@@ -178,6 +178,38 @@ func main() {
 
 	fmt.Println("--------------------------------------")
 
+	// Десериализация произвольного (динамического) объекта
+	var dynamicJson = `{
+		"Name": "Eve",
+		"Age": 6,
+		"Parents": [
+			"Alice",
+			"Bob"
+		]
+	}
+    `
+	var v interface{}
+	json.Unmarshal([]byte(dynamicJson), &v)
+	data := v.(map[string]interface{})
+
+	for k, v := range data {
+		switch v := v.(type) {
+		case string:
+			fmt.Println(k, v, "(string)")
+		case float64:
+			fmt.Println(k, v, "(float64)")
+		case []interface{}:
+			fmt.Println(k, "(array):")
+			for i, u := range v {
+				fmt.Println("    ", i, u)
+			}
+		default:
+			fmt.Println(k, v, "(unknown)")
+		}
+	}
+
+	fmt.Println("--------------------------------------")
+
 	// Десериализация из yaml
 	var tom3 Person
 	tomYaml = []byte(`name: Tom
