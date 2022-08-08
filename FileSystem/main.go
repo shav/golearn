304257bfb99872f836076ef4d503815f19f512be
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2022-08-08 11:04:30 +0400

    working with file system

--- FileSystem/main.go
+++ FileSystem/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -46,5 +47,74 @@ func main() {
 		fmt.Println(err)
 	} else {
 		fmt.Println(rel)
+	}
+
+	fmt.Println("\n--------------------------------------")
+
+	// Создание файлов и папок
+	err = os.Mkdir("subdir", 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//defer os.RemoveAll("subdir")
+
+	createEmptyFile("subdir/file1.txt")
+	os.MkdirAll("subdir/parent/child", 0755)
+	createEmptyFile("subdir/parent/file2.txt")
+	createEmptyFile("subdir/parent/file3.txt")
+	createEmptyFile("subdir/parent/child/file4.txt")
+
+	fmt.Println("\n--------------------------------------")
+
+	// Чтение файловой структуры
+	var folderName = "subdir/parent"
+	folder, err := os.ReadDir(folderName)
+	fmt.Printf("Listing %s:\n", folderName)
+	// Перечисляет содержимое папки нерекурсивно (т.е. без содержимого подпапок)!
+	for _, entry := range folder {
+		fmt.Printf("    %s: %t\n", entry.Name(), entry.IsDir())
+	}
+	fmt.Println()
+
+	folderName = "subdir/parent/child"
+	os.Chdir(folderName)
+	folder, _ = os.ReadDir(".")
+	fmt.Printf("Listing %s:\n", folderName)
+	for _, entry := range folder {
+		fmt.Printf("    %s: %t\n", entry.Name(), entry.IsDir())
+	}
+	fmt.Println()
+
+	// Рекурсивный обход папки
+	folderName = "subdir"
+	os.Chdir("../../..")
+	fmt.Printf("Visiting %s:\n", folderName)
+	filepath.Walk(folderName, visit)
+
+	fmt.Println("\n--------------------------------------")
+
+	// Временные файлы и папки
+	tmpFile, err := os.CreateTemp("", "go-sample-*.txt")
+	// defer os.Remove(tmpFile.Name())
+	fmt.Println("Temp file name: ", tmpFile.Name())
+
+	tmpFolder, err := os.MkdirTemp("", "sampledir")
+	// defer os.RemoveAll(tmpFolder)
+	fmt.Println("Temp dir name: ", tmpFolder)
+}
+
+func visit(entry string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	fmt.Printf("    %s: %t\n", entry, info.IsDir())
+	return nil
+}
+
+func createEmptyFile(name string) {
+	d := []byte("")
+	err := os.WriteFile(name, d, 0644)
+	if err != nil {
+		fmt.Println(err)
 	}
 }
